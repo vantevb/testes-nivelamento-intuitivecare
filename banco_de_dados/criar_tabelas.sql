3c89150962a645c6e91d9911fcb43bc3d743304d
Nathally Machado <nathallyvbm@gmail.com>
2025-03-25 18:51:23 -0300

    Create criar_tabelas.sql

---
+++ banco_de_dados/criar_tabelas.sql
@@ -0,0 +1,8 @@
+-- Desenvolvido por Nathally V. B. Machado
+CREATE TABLE operadoras (
+    codigo_registro INT PRIMARY KEY,
+    nome_fantasia VARCHAR(255),
+    cnpj VARCHAR(20),
+    modalidade VARCHAR(100),
+    uf VARCHAR(2)
+);
